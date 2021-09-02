efbf1be8afbf0ab0d482b2e9bd3992e3c182301c
shahidmonowarr <shahidmonowar21@gmail.com>
2021-09-02 19:34:17 +0600

    fifth commit

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const bookContainer = document.getElementById("book-container");
 const spinner = document.getElementById("spinner");
 const totalResult = document.getElementById("total-result");
 const errorDiv = document.getElementById("error");
-let totalarr = 0;
+
+//function for passing url for data fetching based on different input 
 const searchBook = () => {
   const searchText = searchInput.value;
 
@@ -14,6 +15,8 @@ const searchBook = () => {
 
   // showing spinner 
   spinner.classList.remove("d-none");
+
+  // data fatching
   fetch(`https://openlibrary.org/search.json?q=${searchText}`)
     .then((res) => res.json())
     .then((data) => {
@@ -24,13 +27,15 @@ const searchBook = () => {
       }, 1500);
     });
   searchInput.value = '';
-  ///
+
+  ///releted searching data fatching
   fetch(`https://openlibrary.org/search.json?q=${searchText}`)
     .then(res => res.json())
     .then(items => countSearch(items.numFound))
 }
-
 searchBook();
+
+//function for displaying all product by search after fetching data
 const showData = (books) => {
   //console.log(books);
 
@@ -41,57 +46,34 @@ const showData = (books) => {
     errorDiv.innerText = "";
   }
 
-  /*
-  let totalarr = 0;
-
-  // calculating total result 
-  books.forEach((book) => {
-    totalarr++;
-  })
-  console.log("total: ", totalarr);
-  totalResult.innerHTML = `
-  <h3>Total results founds: ${totalarr} </h3>
-  `;
-  */
-
   //  loading particular book
   books.slice(0, 30).forEach((book) => {
 
-    const div = document.createElement("div");
-    div.classList.add("col-md-3");
-    div.innerHTML = `
-      <!-- Image -->
-      <div class="rounded overflow-hidden bg-light p-2">
-        <img
-          src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"
-          class="w-100"
-          alt=""
-        />
-      </div>
-      <!-- Body -->
-      <div
-        class="
-          py-2
-          d-flex
-          justify-content-between
-          align-items-center
-          d-md-block
-          text-center bg-light 
-        "
-      >
-        <h2>Book Name: ${book.title}</h2>
-        <h3>Author Name: ${book.author_name}</h3>
-        <h3>Publisher: ${book.publisher}</h3>
-        <h4>First Publish: ${book.first_publish_year}</h4>
-      `;
-    bookContainer.appendChild(div);
+
+    const bookDiv = document.createElement("div");
+    bookDiv.className = "col m-auto";
+    let bookCard = ` <div class="m-3" style="cursor: pointer">
+          <div class="card h-100">
+            <img
+              src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg"
+              class="card-img-top img-fluid" style=""
+            />
+            <div class="card-body text-center">
+                    <h4>Book Name: ${book.title}</h4>
+                    <h5>Author Name: ${book.author_name}</h5>
+                    <h6>Publisher: ${book.publisher}</h6>
+                    <h5> <small class="text-muted">First Publish: ${book.first_publish_year}</small></h5>
+        </div>`;
+    bookDiv.innerHTML = bookCard;
+
+    bookContainer.appendChild(bookDiv);
 
   });
 
 }
 
+// function for displaying related search data
 const countSearch = (items) => {
-
   console.log(items);
   totalResult.innerHTML = `
   <h3>Total results founds: ${items} </h3>
