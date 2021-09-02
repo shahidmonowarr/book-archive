d23918246a9f90d4531a373013178840403b70aa
shahidmonowarr <shahidmonowar21@gmail.com>
2021-09-02 11:57:38 +0600

    spinner added

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 const searchInput = document.getElementById("searchInput");
 const bookContainer = document.getElementById("book-container");
+const spinner = document.getElementById("spinner");
 const searchBook = () => {
-    const searchText = searchInput.value;
+  const searchText = searchInput.value;
 
-    fetch(`http://openlibrary.org/search.json?q=${searchText}`)
-        .then(res => res.json())
-        .then(data => showData(data.docs))
-
-
+  bookContainer.innerHTML = '';
+  spinner.classList.remove("d-none");
+  fetch(`https://openlibrary.org/search.json?q=${searchText}`)
+    .then((res) => res.json())
+    .then((data) => {
+      // Setting a timer of 1.5s, before removing the spinnner, and showing data
+      setTimeout(() => {
+        spinner.classList.add("d-none");
+        showData(data.docs);
+      }, 1500);
+    });
+  searchInput.value = '';
 }
 searchBook();
 const showData = (books) => {
-    console.log(books);
+  console.log(books);
 
-    books.forEach((book) => {
-        //console.log(book);
-        const div = document.createElement("div");
-        div.classList.add("col-md-3");
-        div.innerHTML = `
+  books.forEach((book) => {
+    //console.log(book);
+    const div = document.createElement("div");
+    div.classList.add("col-md-3");
+    div.innerHTML = `
       <!-- Image -->
       <div class="rounded overflow-hidden border p-2">
         <img
@@ -42,7 +50,7 @@ const showData = (books) => {
         <h3>Publisher: ${book.publisher}</h3>
         <h4>First Publish: ${book.first_publish_year}</h4>
       `;
-        bookContainer.appendChild(div);
-    });
+    bookContainer.appendChild(div);
+  });
 
 }
